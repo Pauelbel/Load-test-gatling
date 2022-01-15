b0147aff258d4c2debe10e49f920b6d6916e0403
pauelbel <pauelbel@gmail.com>
2022-01-15 12:03:47 +0300

    готов тест стабильности

--- src/test/scala/Perfomance/Stability.scala
+++ src/test/scala/Perfomance/Stability.scala
@@ -5,14 +5,25 @@ import io.gatling.http.Predef._
 import io.gatling.core.controller.inject.open
 
 
+
+// по результатам тестов максимальной производительности (sla на response time 3 сек)
+ 
+
 class Stability extends Simulation {
 
   setUp(
     CommonScenario().inject(
-      rampUsersPerSec(0) to 5.toInt during 30, //разгон 5ч/с 30 мин
-      constantUsersPerSec(5.toInt) during 30 //полка 5ч/с 5 мин
+      rampConcurrentUsers(0) to 30 during 60, // от 0 до 30 пользователей за 600 секунд
+      constantConcurrentUsers(30) during 1800 // удерживать 30 пользователей с течение часа
     )
   ).protocols(httpProtocol)
-    .maxDuration(35) //длительность теста = разгон + полка
-
+   .throttle(                      // ограничение по рпс
+     reachRps(40) in (60),        // поднять рпс до 40 за 60 секунд
+     holdFor(1800)                 // удерживать рпс 1800 секунд
+    ).maxDuration(3000)
 }
+  
+  
+  
+  
+  
